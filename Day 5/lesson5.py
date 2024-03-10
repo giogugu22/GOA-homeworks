09ac2b016dc15098f68d314cd3ec670b203dbc33
giogugu <giorgigugushvili222@gmail.com>
2024-03-10 17:35:13 +0400

    second commit

---
+++ Day 5/lesson5.py
@@ -0,0 +1,25 @@
+
+
+# /
+
+print(5 / 2)
+print(10 / 2)
+print(8 / 2)
+print(6 / 2)
+
+
+
+# //
+
+print(7 // 2)
+print(9 // 2)
+print(11 // 2)
+
+
+
+
+# %
+
+print(5 % 4)
+print(7 % 2)
+print(9 % 3)
