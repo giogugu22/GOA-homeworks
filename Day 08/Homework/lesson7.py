d4883305443c28ef707ae324f3b1c7189eb5bf28
giogugu <giorgigugushvili222@gmail.com>
2024-03-25 21:45:19 +0400

    commit

---
+++ Day 08/Homework/lesson7.py
@@ -0,0 +1,10 @@
+temp_in_celsius = 10
+temp_in_fahrenheit = (temp_in_celsius * 1.8) + 32
+
+print(temp_in_fahrenheit)
+
+
+temp_in_celsius = float(input("Please enter temp in celsius: "))
+temp_in_fahrenheit = (temp_in_celsius * 1.8) + 32
+
+print(temp_in_fahrenheit)  
