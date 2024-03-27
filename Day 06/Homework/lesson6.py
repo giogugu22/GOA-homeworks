82dce9b447d2bafe0be1dae7f12b328bd7d70894
giogugu <giorgigugushvili222@gmail.com>
2024-03-27 14:28:27 +0400

    commit

--- Day 06/Homework/lesson6.py
+++ Day 06/Homework/lesson6.py
@@ -1,49 +1,83 @@
-#>
-10>5
-7>2
-8>3
-12>7
-16>3
+#1
+x=int(input("enter a number "))
+y= float(x)
+print(y==x)
 
-#>=
-10>=10
-15>=5
-7>=4
-8>=8
-43>=15
 
-#<
-5<7
-10<12
-4<40
-7<17
-2<4
 
-#<=
-4<=7
-2<=8
-5<=7
-2<=2
-7<=7
 
-#==
-10==10
-5==5
-2==2
-7==7
-13==13
+#> 2
+print(10>5)
+print(7>2)
+print(8>3)
+print(12>7)
+print(16>3)
 
-#!=
-5!=9
-2!=5
-4!=6
-1!=8
-6!=3
 
-#
-10<100
-100>10
-50<=500
-20!=200
-100>=100
+#>= 3
+print(10>=10)
+print(15>=5)
+print(7>=4)
+print(8>=8)
+print(43>=15)
 
+
+#< 4
+print(5<7)
+print(10<12)
+print(4<40)
+print(7<17)
+print(4<2)
+
+
+#<= 5
+
+print(7<=4)
+print(2<=8)
+print(5<=7)
+print(2<=2)
+print(7<=7)
+
+
+#== 6
+print(10==10)
+print(5==5)
+print(2==2)
+print(7==3)
+print(13==13)
+
+
+#!= 7
+print(5!=9)
+print(2!=2)
+print(4!=6)
+print(1!=8)
+print(6!=3)
+
+
+# 8
+x=int(input("enter your number "))
+y=int(input("enter your number "))
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==y)
+print(x>=y)
+print(x<=y)
+
+ #9
+
+x=int(input("enter your number "))
+y=x+10
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==y)
+print(x>=y)
+print(x<=y)
+
+#10
+x=int(input("enter your number "))
+y= float(input("enter your number "))
+print(x!=y)
+print(x==y)
--- Day 07/Homework/lesson7.py
+++ Day 07/Homework/lesson7.py
@@ -1,10 +1,19 @@
+#1
 temp_in_celsius = 10
 temp_in_fahrenheit = (temp_in_celsius * 1.8) + 32
-
 print(temp_in_fahrenheit)
 
+#2
+age=int(input("enter your number "))
+print(age>12)
+print(age<20)
 
-temp_in_celsius = float(input("Please enter temp in celsius: "))
-temp_in_fahrenheit = (temp_in_celsius * 1.8) + 32
+#3
+lenght=float(input("enter your number "))
+width=float(input("enter your number "))
+S=lenght*width
+print(S)
 
-print(temp_in_fahrenheit)  
+#4
+number=int(input("enter your number "))
+print(number>100 and number%9==0)
