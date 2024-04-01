3506c768c6b9652c2ddc4bcb05c222bb5c39c046
giogugu <giorgigugushvili222@gmail.com>
2024-04-01 23:06:54 +0400

    commit

--- Day 09/Homework/lesson9.py
+++ Day 09/Homework/lesson9.py
@@ -1,11 +1,11 @@
 salary=int(input("enter your number "))
-if salary>10000
-print("გოაში სწავლობდი?")
+if salary>10000:
+    print("გოაში სწავლობდი?")
+
+ 
+elif 1000<salary and salary<10000 :
+    print("you mid")
 
 else: 
-if 1000<salary
-salary<10000 
-print("you mid")
-
-else: salary<1000
-print("შემოსულიყავი გოაში, მატრიცელო")
+    salary<1000
+    print("შემოსულიყავი გოაში, მატრიცელო")
