80fd8e1619c93ea2c72a2d437a5c3eb4ea0083c0
giogugu <giorgigugushvili222@gmail.com>
2024-04-01 17:06:19 +0400

    commit

---
+++ Day 09/Homework/lesson9.py
@@ -0,0 +1,11 @@
+salary=int(input("enter your number "))
+if salary>10000
+print("გოაში სწავლობდი?")
+
+else: 
+if 1000<salary
+salary<10000 
+print("you mid")
+
+else: salary<1000
+print("შემოსულიყავი გოაში, მატრიცელო")
