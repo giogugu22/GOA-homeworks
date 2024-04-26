7181dc56ae8e636877bae27b780acc90807cfa88
giogugu <giorgigugushvili222@gmail.com>
2024-04-26 18:21:27 +0400

    commit

---
+++ Day 12/Homework/lesson12.py
@@ -0,0 +1,18 @@
+#1
+for i in range(0, 10+0):
+    if i%2==1:
+        print (i)
+        
+#2
+for i in range(0, 50+0):
+    if i%5==0:
+        print (i)
+
+#3
+Customer=int(input("Enter your number: "))
+for i in range(0, Customer):
+    print (i)
+
+#4
+for i in range(0, 50+0):
+    sum(1, 50)
