9af1b61a61f7aab2f27a554d2323092aa472fc9f
giogugu <giorgigugushvili222@gmail.com>
2024-05-22 19:37:07 +0400

    commit

---
+++ SkillWill/HW2.py
@@ -0,0 +1,19 @@
+#my_list_int = [1, 2, 3, 4, 5]
+#my_list_str = ["a", "b", "c", "d"]
+#my_list_mix = [1, 4.0, "b", False]
+#print(my_list_int)
+#my_tuple = (1, 2, 3, 4, 5, 6)
+#print(my_tuple[1])
+#my_dict = {"name" : "Gio", "last name" : "Gugusha"}
+#my_set = {1, 2, 3, 4, 1}
+#print(my_set)
+
+#1
+def contact(name, number):
+    phonenumber = {'name': number, 'phone':number}
+    return phonenumber
+
+    
+    
+
+
